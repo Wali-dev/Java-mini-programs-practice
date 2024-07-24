1e84ed3a46a3b6ba76a5a46629cdb34c223130e4
Wali Cornell <cornelhassan780@gmail.com>
2024-07-24 16:50:18 +0530

    practice

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class SimpleCalculator{
-    public static void main(String[] args){
+public class SimpleCalculator {
+    public static void main(String[] args) {
 
         System.out.println("Please enter two numbers: ");
         Scanner input = new Scanner(System.in);
@@ -9,12 +9,26 @@ public class SimpleCalculator{
         float num1 = input.nextFloat();
         float num2 = input.nextFloat();
 
+        float res = 0;
+
         System.out.println("Enter the operation(+ , - , *, /): ");
         Scanner op = new Scanner(System.in);
+        char oparend = op.next().trim().charAt(0);
 
-        String oparend = op.next();
-System.out.println("");
+        if (oparend == '+' || oparend == '-' || oparend == '*' || oparend == '/') {
+            if (oparend == '+') {
+                res = num1 + num2;
+            } else if (oparend == '-') {
+                res = num1 - num2;
+            } else if (oparend == '*') {
+                res = num1 * num2;
+            } else if (oparend == '/') {
+                res = num1 / num2;
+            }
+        } else {
+            System.out.println("Enter a valid operator and run the program again");
+        }
 
-        
+        System.out.println(res);
     }
 }
